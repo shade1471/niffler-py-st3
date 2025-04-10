d87ae71ecd11a3732e614cc2d645bb83f8941982
a.gribanov <a.gribanov@staffcop.ru>
2025-04-10 15:15:03 +0700

    add method to spend_db

--- python_test/databases/spend_db.py
+++ python_test/databases/spend_db.py
@@ -17,12 +17,17 @@ class SpendDb:
             statement = select(Category).where(Category.username == username)
             return session.exec(statement).all()
 
+    def get_category_by_id(self, category_id: str) -> Category:
+        with Session(self.engine) as session:
+            statement = select(Category).where(Category.id == category_id)
+            return session.exec(statement).first()
+
     def delete_category(self, category_id: str):
         with Session(self.engine) as session:
             category = session.get(Category, category_id)
             session.delete(category)
             session.commit()
 
-    def delete_categories_by_ids(self, categories_ids: list):
+    def delete_categories_by_ids(self, categories_ids: list[str]):
         for category_id in categories_ids:
             self.delete_category(category_id)
